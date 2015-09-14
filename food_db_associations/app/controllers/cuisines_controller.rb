f2b29c1da0c9401f5820669a9b5a8600c672334b
Joshua Swiss <swissjoshua@gmail.com>
2015-09-14 08:55:26 +0100

    delete working

--- food_db_associations/app/controllers/cuisines_controller.rb
+++ food_db_associations/app/controllers/cuisines_controller.rb
@@ -9,11 +9,13 @@ class CuisinesController < ApplicationController
 	end
 
 	def create
-		@cuisine = cuisine.new(cuisine_params)
+		
+		# cuisine = Cuisine.new(cuisine_params)
+		@cuisine = Cuisine.new(name: params[:cuisine]['name'], region: params[:cuisine]['region'], description: params[:cuisine]['description'])
 
-		if cuisine.save
+		if @cuisine.save
 			flash[:notice] = 'You added a new cuisine!'
-			redirect_to cuisine
+			redirect_to cuisines_path(@cuisines)
 		else
 			flash.now[:error] = "Error: #{cuisine.errors.full_messages}"
 			render :new
@@ -25,12 +27,22 @@ class CuisinesController < ApplicationController
 	end
 
 	def edit
+		@cuisine = Cuisine.find(params[:id])		
 	end
 
 	def update
+		cuisine = Cuisine.find(params[:id])
+
+		if cuisine.update(name: params[:cuisine]['name'], region: params[:cuisine]['region'], description: params[:cuisine]['description'])
+			redirect_to cuisine
+		else
+			render :edit
+		end
 	end
 
 	def destroy
+		@cuisine = Cuisine.find(params[:id]).destroy
+		redirect_to root_path
 	end
 
 end
